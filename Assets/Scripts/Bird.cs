3283f4c23206b5c27bac919f4c08422379c17188
Roman <romaaverkin@gmail.com>
2021-05-12 16:17:25 +0700

    Создал одну двигающуюся трубу

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float force;
+    Rigidbody2D BirdRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BirdRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BirdRigid.velocity = Vector2.up * force;
+        }
+    }
+}
